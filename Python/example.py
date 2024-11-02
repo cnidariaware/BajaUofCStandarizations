d7d562c297d397c3fce767491fd1ad50cfd79205
darkicewolf50 <95242911+darkicewolf50@users.noreply.github.com>
2024-11-02 16:02:40 -0400

    fix(python example): made it so that the vs code tooltip works/looks properly

--- Python/example.py
+++ Python/example.py
@@ -1,12 +1,15 @@
-def example_funct(input_var):
+def example_funct(input_var:type):
     """ 
     What function does
-    REQUIRES:
-        input var type
-    PROMISES:
-        What function returns if applicable
-    Develop in part by: Your Name
-    Contact: semi-perminant email, do not need to respond but try to be a good alumni
+
+    ``REQUIRES``: input var type
+    
+    ``PROMISES``: What function returns if applicable
+
+    ``Develop in part by``: Your Name
+
+    ``Contact``: semi-perminant email, do not need to respond but try to be a good alumni
+
     """
     
     var_example = 0     #example variable can be anyhting not just an int
